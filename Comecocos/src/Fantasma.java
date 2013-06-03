5ece543e6acb15ba4c5ff409d3bfce05927210dc
susanadnv <su.donasci@gmail.com>
2013-06-03 14:31:27 +0200

    cambio

--- Comecocos/src/Fantasma.java
+++ Comecocos/src/Fantasma.java
@@ -3,8 +3,40 @@ public class Fantasma extends Personaje{
 
 	enum Estado { FANTASMA, HUIR, RETORNAR, ENTRAR, ESPERAR, SALIR }
 	
-	public Fantasma(double velocidad, double xObjetivo, double yObjetivo) {
+	private Estado estado;
+	
+	public Fantasma( NombresPersonaje id, Laberinto laberinto) {
+		super(id, laberinto);
+	}
+	
+	public void mover(double velocidad, int xObj, int yObj) {
+		/*estado ENTRAR, ESPERAR O SALIR*/
+		if (this.estado==Estado.ENTRAR){
+			
+		} 
+		else if (this.estado==Estado.ESPERAR ){
+			
+		}
+		else if (this.estado==Estado.SALIR) {
+			
+		}
+		else {
+			super.mover(velocidad);
+			// cambiar dirección si se puede en función del objetivo
+			if (){
+				
+			}
+		}
 		
 	}
+	
+	public Estado getEstado(){
+		return this.estado;
+	}
+	
+	public void setEstado(Estado estado){
+		this.estado=estado;
+	}
+	
 	
 }
