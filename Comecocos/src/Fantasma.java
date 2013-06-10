76e60d7f258afa2960168602a707ce86cc428045
susanadnv <su.donasci@gmail.com>
2013-06-10 14:07:08 +0200

    cambios fantasma direccion

--- Comecocos/src/Fantasma.java
+++ Comecocos/src/Fantasma.java
@@ -1,3 +1,5 @@
+import java.awt.geom.Point2D;
+
 
 public class Fantasma extends Personaje{
 
@@ -23,9 +25,53 @@ public class Fantasma extends Personaje{
 		else {
 			super.mover(velocidad);
 			// cambiar dirección si se puede en función del objetivo
-			if (){
-				
+			double min = Double.MAX_VALUE;
+			double dis;
+			Point2D.Double p=getPosicion();
+			Direccion nuevaDireccion=getDireccion();
+			
+			/*invocamos metodo para ver si se puede cambiar direccion y comprobamos que no sea la contraria
+			calculo de distancia al objetivo y adjudicamos nueva direccion*/
+			
+			if (getDireccion()!=Direccion.ABAJO && puedeCambiarDireccion(Direccion.ARRIBA)) {
+
+				dis=Math.sqrt(Math.pow(xObj-p.x, 2)+Math.pow(yObj-p.y-35, 2));
+				if(dis<min){
+					min=dis;
+					nuevaDireccion=Direccion.ARRIBA;
+				}
+			
 			}
+			else if (getDireccion() !=Direccion.ARRIBA && puedeCambiarDireccion(Direccion.ABAJO)) {
+
+				dis=Math.sqrt(Math.pow(xObj-p.x, 2)+Math.pow(yObj-p.y+35, 2));
+				if(dis<min){
+					min=dis;
+					nuevaDireccion=Direccion.ABAJO;
+				}
+			
+			}
+			else if (getDireccion() !=Direccion.IZDA && puedeCambiarDireccion(Direccion.DERECHA)) {
+
+				dis=Math.sqrt(Math.pow(xObj-p.x+35, 2)+Math.pow(yObj-p.y, 2));
+				if(dis<min){
+					min=dis;
+					nuevaDireccion=Direccion.DERECHA;
+				}
+			
+			}
+			else if (getDireccion() !=Direccion.DERECHA && puedeCambiarDireccion(Direccion.IZDA)) {
+
+				dis=Math.sqrt(Math.pow(xObj-p.x-35, 2)+Math.pow(yObj-p.y, 2));
+				if(dis<min){
+					min=dis;
+					nuevaDireccion=Direccion.IZDA;
+				}
+			
+			}		
+			//Antes de cambiar la direccion, comprobamos que sean diferentes
+			if (nuevaDireccion!=getDireccion())
+				setDireccion(nuevaDireccion);
 		}
 		
 	}
